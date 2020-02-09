12d92f070fdc28003c63a05a80a029ef7c4ade71
Pranav Burugula <prb3@illinois.edu>
2020-02-08 20:03:26 -0600

    ML integration

--- model.py
+++ model.py
@@ -1,6 +1,19 @@
 import numpy as np
-import keras
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 
 class LSTMModel(object):
-    def __init__(self, )
+    def __init__(self, json, weights):
+        json_file = open(json, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights(weights)
+        loaded_model.compile(loss='mean_absolute_percentage_error', optimizer='rmsprop', metrics=['accuracy'])
+        self.model = loaded_model
+    
+    def get_prediction(self, sequence):
+        prediction = self.model.predict_on_batch(sequence)
+        return prediction
     pass
--- tcpserver.py
+++ tcpserver.py
@@ -1,9 +1,13 @@
 import time
+import numpy as np
 import BaseHTTPServer
+from model import LSTMModel
 
 HOST_NAME = '192.168.4.1' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 8000 # Maybe set this to 9000.
-value = ""
+value = np.zeros(2)
+model = LSTMModel("Model2.json", "Model2.h5")
+start_time = time.time()
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
         s.send_response(200)
@@ -14,10 +18,16 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_response(200)
         s.send_header("Content-type", "text/plain")
         s.end_headers()
+        global model
         global value
-        print "GET request,\nPath: ", str(s.path),"\nHeaders:\n", str(s.headers), "\n\nBody:\n",value, "\n"
-        
-        s.wfile.write(value)
+        predicted = model.get_prediction(value)
+        print "Predicted: ", predicted[0], ", ", predicted[1]
+        if time.time() >= predicted[1]:
+            s.wfile.write(predicted[0])
+            print "GET request,\nPath: ", str(s.path),"\nHeaders:\n", str(s.headers), "\n\nBody:\n",predicted[0], "\n"
+        else:
+            s.wfile.write(value[0])
+            print "GET request,\nPath: ", str(s.path),"\nHeaders:\n", str(s.headers), "\n\nBody:\n",value[0], "\n"
         # If someone went to "http://something.somewhere.net/foo/bar/",
         # then s.path equals "/foo/bar/".
     def do_POST(s):
@@ -25,11 +35,15 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         post_data = s.rfile.read(content_length) # <--- Gets the data itself
         print "POST request,\nPath: ",str(s.path),"\nHeaders:\n", str(s.headers), "\n\nBody:\n", post_data.decode('utf-8'), "\n"
         global value
-        value = post_data.decode('utf-8')
+        global start_time
+        value_str = post_data.decode('utf-8')
+        value[0] = int(value_str)
+        value[1] = time.time() - start_time
         s.send_header("Content-type", "text/plain")
         s.end_headers()
         s.wfile.write("Success")
 if __name__ == '__main__':
+    
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
     print (time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
